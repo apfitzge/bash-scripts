00cb0f7f7c071c29821e7f7803d543a97a561ee0
Andrew Fitzgerald <apfitzge@gmail.com>
2023-04-07 18:34:14 +0000

    actually empty body, seems like a bug in gh cli - or i'm dumb

--- scripts.sh
+++ scripts.sh
@@ -65,7 +65,7 @@ sol_merge() {
     sol_checks && \
     git checkout "$SOL_MERGE_OG_BRANCH" && \
     gh pr checks "$1" && \
-    gh pr merge -sdb "" "$1"
+    gh pr merge -sd "$1" --body ""
 
     git branch -D "$SOL_MERGE_MG_BRANCH_REBASE"
     git checkout "$SOL_MERGE_OG_BRANCH" # fall-back in case of failure - switch back to original branch
