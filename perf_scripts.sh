0cda894cf0d7a0141105b4d10c3fef9ffbe4465b
Andrew Fitzgerald <apfitzge@gmail.com>
2026-09-10 18:35:50 +0000

    perf

--- perf_scripts.sh
+++ perf_scripts.sh
@@ -2,3 +2,9 @@ function get_thread_pid() {
     local thread_name="$1"
     ps -eL | grep -w "$thread_name" | awk '{print $2}' | head -n 1
 }
+
+function profile_env() {
+    export RUSTFLAGS="-C debuginfo=2 -C force-frame-pointers=yes"
+    export CFLAGS="-fno-omit-frame-pointer -g"
+    export CXXFLAGS="-fno-omit-frame-pointer -g"
+}
